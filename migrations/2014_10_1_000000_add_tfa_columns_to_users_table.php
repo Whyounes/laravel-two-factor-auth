5b5d7d003c6c814e35397a5377a3ffff68badd2e
RAFIE Younes <younes.rafie@gmail.com>
2016-12-17 12:06:41 +0000

    Fixed migrations bug

--- migrations/2014_10_1_000000_add_tfa_columns_to_users_table.php
+++ migrations/2014_10_1_000000_add_tfa_columns_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class AddTfaColumnsToUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,11 +14,11 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            if(!Schema::hasColumn('country_code')) {
+            if(!Schema::hasColumn('users', 'country_code')) {
                 $table->string('country_code', 4)->nullable();
             }
 
-            if(!Schema::hasColumn('phone')) {
+            if(!Schema::hasColumn('users', 'phone')) {
                 $table->string('phone')->nullable();
             }
         });
